6af4e00a2fa61e07456f29952a763fb50a11f526
choojunghun <choojunghun@naver.com>
2021-05-19 06:24:05 +0900

    second.py is updated by choojunghun!:

--- second.py
+++ second.py
@@ -23,3 +23,34 @@ search_btn.click()
 # print(dust_name1)
 #
 #이은희 크롤러 코딩
+
+dust_table=driver.find_element_by_class_name("tb_scroll").text
+print(dust_table)
+
+with urlopen('https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&qvt=0&query=%EC%A0%84%EA%B5%AD%EC%B4%88%EB%AF%B8%EC%84%B8%EB%A8%BC%EC%A7%80') as response:
+    soup = BeautifulSoup(response,'html.parser')
+
+    dust=soup.find('div',{'class':'tb_scroll'})
+
+    dust_n = dust.select('th')
+    dust_na=[]
+    for i in dust_n :
+        dust_na.append(i.text)
+    dust_name=[]
+    for i in range(len(dust_na)):
+        if i>3:
+            dust_name.append(dust_na[i])
+    # print(dust_name)
+
+    dust_t=dust.select("td")
+    dust_td=[]
+    dust_cc = []
+    for i in dust_t:
+        dust_td.append(i.text)
+    for i in range(len(dust_td)):
+        if i%3==0:
+            k=int(dust_td[i])
+            dust_cc.append(k)
+    # print(dust_cc)
+
+#추정훈 크롤러 코딩
